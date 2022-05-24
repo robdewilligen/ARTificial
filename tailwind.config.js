d2bb8347a1c296522af423600e9ff00b419b8a41
memyselfandmatthijs <matthijsvan3ck@gmail.com>
2022-05-24 11:34:14 +0200

    added the colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
+    colors: {
+      'LogoGreen': '#058c42',
+      'BlackLogo': '232323',
+      'whiteText': 'FFFFFF',
+      'background': '#7b9568',
+      'buttonsAndMenu': '#000000'
+    },
     extend: {},
   },
   plugins: [],
