d67767bfccfe885d77e58e96bec91e3ed9ed341e
memyselfandmatthijs <matthijsvan3ck@gmail.com>
2022-05-24 11:43:24 +0200

    moved colors to the right file

--- artificial/tailwind.config.js
+++ artificial/tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
+    colors: {
+      'LogoGreen': '#058c42',
+      'BlackLogo': '232323',
+      'background': '#7b9568',
+      'buttonsAndMenu': 'c4c4c4'
+    },
     extend: {},
   },
   plugins: [],
