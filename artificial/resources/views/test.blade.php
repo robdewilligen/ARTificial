b18ec2c4b5bbf17837366bd36a497fb980012336
Rob de Willigen <robdewilligen@outlook.com>
2022-06-01 12:26:47 +0200

    fix alle BS

--- artificial/resources/views/test.blade.php
+++ artificial/resources/views/test.blade.php
@@ -7,12 +7,8 @@
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-<<<<<<< HEAD
-  <h1 class="text-red-500 font-sans">
-=======
 @include('layouts.navbar')
   <h1 class="bg-background-cl w-10">
->>>>>>> 071fed4a906b3f378d6923e615148378a7f34cb0
     Hello world!
   </h1>
 </body>
--- artificial/tailwind.config.js
+++ artificial/tailwind.config.js
@@ -9,21 +9,10 @@ module.exports = {
         'Bungee': 'cursive'
     },
     colors: {
-<<<<<<< HEAD
-      'LogoGreen': '#058c42',
-      'BlackLogo': '#232323',
-      'backgroundColor': '#7b9568',
-      'ButtonsAndMenu': '#c4c4c4',
-      'white': '#ffffff'
-    },
-    fontFamily: {
-      'sans': ['Bungee'],
-=======
       'logoGreen': '#058c42',
       'blackLogo': '#232323',
       'background-cl': '#7b9568',
       'buttonsAndMenu': '#c4c4c4'
->>>>>>> 071fed4a906b3f378d6923e615148378a7f34cb0
     },
     extend: {},
   },
