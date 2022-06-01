d529baa37b0adcc645ef6820775dbd1f3ab7b40b
memyselfandmatthijs <matthijsvan3ck@gmail.com>
2022-06-01 19:50:27 +0200

    changed the homepage

--- artificial/resources/views/home.blade.php
+++ artificial/resources/views/home.blade.php
@@ -7,13 +7,16 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Home</title>
 </head>
-<body class="bg-backgroundColor">
-    @include('layouts.navbar')
-    <img src="#" >
+<body class="bg-background-cl px-7">
 
-    <h1 class="text-3xl text-white">Welkom!</h1>
+    <img src="#" alt="logo" >
 
-    <p class="text-white"></p>
+    <h1 class="text-3xl text-white text-center">Hey pik!</h1>
+
+    <p class="text-white text-center" >Welkom bij kunststroom</p>
+    <br>
+    <p class="text-white text-center">Kunststroom is een project waarmee de biodiversiteit gestimuleerd word om terug te keren naar de binnenstad van Rotterdam. </p>
     
+    <button class="bg-buttonsAndMenu px-3 rounded-sm">kunststroom</button>
 </body>
 </html>
