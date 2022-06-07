c9a95544791598ed820158813d9819dff8d03585
Rob de Willigen <robdewilligen@outlook.com>
2022-06-07 11:37:58 +0200

    Relocate Menu + create AboutUsPage

--- artificial/resources/views/aboutUs.blade.php
+++ artificial/resources/views/aboutUs.blade.php
@@ -8,8 +8,17 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>About us Page</title>
 </head>
-<body>
+<body class="bg-background-cl px-7">
 @include('layouts.navbar')
+
+<h1 class="text-3xl text-white text-center">Wie zijn wij?</h1>
+
+<p class="text-white text-center" >Welkom bij kunststroom</p>
+<br>
+<p class="text-white text-center">ARTificial is een start-up opgericht door 4 studenten van de Hogeschool Rotterdam van de opleiding Creative media & game technologies.
+    Deze start-up is ontstaan door een project waarbij Er gekeken moest worden naar hoe er door middel van technologie, een steentje bijgedragen kan worden aan de duurzaamheid van Rotterdam.
+
+    Voor dit project is Kunststroom bedacht.</p>
 
 </body>
 </html>
