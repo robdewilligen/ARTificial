4872ee445eda923cd1a9eddab61f04056fa89ef8
memyselfandmatthijs <matthijsvan3ck@gmail.com>
2022-06-14 10:41:01 +0200

    changed webpack

--- artificial/webpack.mix.js
+++ artificial/webpack.mix.js
@@ -14,7 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/index.js', 'public/js')
     .js('resources/js/tips.js', 'public/js')
-    .js('resources/js/charts.js')
+    .js('resources/js/charts.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require("tailwindcss"),
     ]);
